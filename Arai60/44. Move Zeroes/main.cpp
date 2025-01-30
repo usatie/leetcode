f24aaf1c13c4b2fc1c6d935297d7a318e5b82dda
Shun Usami <usatie@gmail.com>
2025-01-30 11:58:15 -0800

    Solved 283. Move Zeroes

---
+++ Arai60/44. Move Zeroes/main.cpp
@@ -0,0 +1,27 @@
+#include "step3.cpp"
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+void test(std::initializer_list<int> input,
+          std::initializer_list<int> expected) {
+  std::vector<int> nums(input);
+  std::vector<int> expected_nums(expected);
+  std::cout << "input: ";
+  for (auto num : nums) {
+    std::cout << num << " ";
+  }
+  Solution().moveZeroes(nums);
+  // vectorに== operatorが使えるって知らなかった
+  // https://en.cppreference.com/w/cpp/container/vector/operator_cmp
+  assert(nums == expected_nums);
+  std::cout << " ===== OK =====" << std::endl;
+}
+
+int main() {
+  test({0, 1, 0, 3, 12}, {1, 3, 12, 0, 0});
+  test({0}, {0});
+  test({0, 0, 0, 0, 0}, {0, 0, 0, 0, 0});
+  test({1, 2, 3, 4, 5}, {1, 2, 3, 4, 5});
+}
---
+++ Arai60/44. Move Zeroes/step1.cpp
@@ -0,0 +1,22 @@
+/*
+  何がわからなかったか
+   - N/A
+  何を考えて解いていたか
+   1. In place -> swapを使いたい
+   2. Use two pointers, one for next insert position, the other for cursor
+   3. When it == next_it, swap is not necessary but it's okay
+  正解してから気づいたこと
+   - N/A
+*/
+class Solution {
+public:
+  void moveZeroes(vector<int> &nums) {
+    auto next_it = nums.begin();
+    for (auto it = nums.begin(); it < nums.end(); ++it) {
+      if (*it != 0) {
+        std::iter_swap(next_it, it);
+        ++next_it;
+      }
+    }
+  }
+};
